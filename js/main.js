4e1e94cc07845db1d687142407a0455ee2616beb
André Guerra <andreguerra7@hotmail.com>
2017-07-03 19:38:08 -0300

    added choose god

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 //Vars
 var statsPoints = 5;
+var god = "none";
 
 //scrollify
 $(function() {
@@ -14,6 +15,135 @@ touchScroll:true,
 document.getElementById("btnNewGame").onclick = function(){
 		$("#divHome").fadeOut("slow","linear");
 		$("#divNewGame").fadeIn("slow","linear");
+}
+//New Game God
+//life
+document.getElementById("btnNewGameLife").onclick = function (){
+	$("#btnGodLife").removeClass("btnGodNotSelected");
+	$("#btnGodLife").addClass("btnGodSelected");
+	$("#btnGodLife").css("color","#ffe884");
+	god = "Life";
+	$("#btnGodDeath, #btnGodOrder, #btnGodChaos, #btnGodWater, #btnGodAir, #btnGodFire, #btnGodEarth").css("color","white");
+	$("#btnGodDeath").attr("class","fa fa-2x fa-low-vision btnGodNotSelected");
+	$("#btnGodOrder").attr("class","fa fa-2x fa-balance-scale btnGodNotSelected");
+	$("#btnGodChaos").attr("class","fa fa-2x fa-certificate btnGodNotSelected");
+	$("#btnGodWater").attr("class","fa fa-2x fa-tint btnGodNotSelected");
+	$("#btnGodAir").attr("class","fa fa-2x fa-cloud btnGodNotSelected");
+	$("#btnGodFire").attr("class","fa fa-2x fa-fire btnGodNotSelected");
+	$("#btnGodEarth").attr("class","fa fa-2x fa-tree btnGodNotSelected");
+	$("#divNewGameGodInfo").html("You've chosen: "+" "+god);
+}
+//death
+document.getElementById("btnNewGameDeath").onclick = function (){
+	$("#btnGodDeath").removeClass("btnGodNotSelected");
+	$("#btnGodDeath").addClass("btnGodSelected");
+	$("#btnGodDeath").css("color","#381172");
+	god = "Death";
+	$("#btnGodLife, #btnGodOrder, #btnGodChaos, #btnGodWater, #btnGodAir, #btnGodFire, #btnGodEarth").css("color","white");
+	$("#btnGodLife").attr("class","fa fa-2x fa-globe btnGodNotSelected");
+	$("#btnGodOrder").attr("class","fa fa-2x fa-balance-scale btnGodNotSelected");
+	$("#btnGodChaos").attr("class","fa fa-2x fa-certificate btnGodNotSelected");
+	$("#btnGodWater").attr("class","fa fa-2x fa-tint btnGodNotSelected");
+	$("#btnGodAir").attr("class","fa fa-2x fa-cloud btnGodNotSelected");
+	$("#btnGodFire").attr("class","fa fa-2x fa-fire btnGodNotSelected");
+	$("#btnGodEarth").attr("class","fa fa-2x fa-tree btnGodNotSelected");
+	$("#divNewGameGodInfo").html("You've chosen: "+" "+god);
+}
+//order
+document.getElementById("btnNewGameOrder").onclick = function (){
+	$("#btnGodOrder").removeClass("btnGodNotSelected");
+	$("#btnGodOrder").addClass("btnGodSelected");
+	$("#btnGodOrder").css("color","#b7a8bf");
+	god = "Order";
+	$("#btnGodLife, #btnGodDeath, #btnGodChaos, #btnGodWater, #btnGodAir, #btnGodFire, #btnGodEarth").css("color","white");
+	$("#btnGodLife").attr("class","fa fa-2x fa-globe btnGodNotSelected");
+	$("#btnGodDeath").attr("class","fa fa-2x fa-low-vision btnGodNotSelected");
+	$("#btnGodChaos").attr("class","fa fa-2x fa-certificate btnGodNotSelected");
+	$("#btnGodWater").attr("class","fa fa-2x fa-tint btnGodNotSelected");
+	$("#btnGodAir").attr("class","fa fa-2x fa-cloud btnGodNotSelected");
+	$("#btnGodFire").attr("class","fa fa-2x fa-fire btnGodNotSelected");
+	$("#btnGodEarth").attr("class","fa fa-2x fa-tree btnGodNotSelected");
+	$("#divNewGameGodInfo").html("You've chosen: "+" "+god);
+}
+//Chaos
+document.getElementById("btnNewGameChaos").onclick = function (){
+	$("#btnGodChaos").removeClass("btnGodNotSelected");
+	$("#btnGodChaos").addClass("btnGodSelected");
+	$("#btnGodChaos").css("color","#c41947");
+	god = "Chaos";
+	$("#btnGodLife, #btnGodOrder, #btnGodDeath, #btnGodWater, #btnGodAir, #btnGodFire, #btnGodEarth").css("color","white");
+	$("#btnGodLife").attr("class","fa fa-2x fa-globe btnGodNotSelected");
+	$("#btnGodOrder").attr("class","fa fa-2x fa-balance-scale btnGodNotSelected");
+	$("#btnGodDeath").attr("class","fa fa-2x fa-low-vision btnGodNotSelected");
+	$("#btnGodWater").attr("class","fa fa-2x fa-tint btnGodNotSelected");
+	$("#btnGodAir").attr("class","fa fa-2x fa-cloud btnGodNotSelected");
+	$("#btnGodFire").attr("class","fa fa-2x fa-fire btnGodNotSelected");
+	$("#btnGodEarth").attr("class","fa fa-2x fa-tree btnGodNotSelected");
+	$("#divNewGameGodInfo").html("You've chosen: "+" "+god);
+}
+//Water
+document.getElementById("btnNewGameWater").onclick = function (){
+	$("#btnGodWater").removeClass("btnGodNotSelected");
+	$("#btnGodWater").addClass("btnGodSelected");
+	$("#btnGodWater").css("color","#2a83f7");
+	god = "Water";
+	$("#btnGodLife, #btnGodOrder, #btnGodChaos, #btnGodDeath, #btnGodAir, #btnGodFire, #btnGodEarth").css("color","white");
+	$("#btnGodLife").attr("class","fa fa-2x fa-globe btnGodNotSelected");
+	$("#btnGodOrder").attr("class","fa fa-2x fa-balance-scale btnGodNotSelected");
+	$("#btnGodChaos").attr("class","fa fa-2x fa-certificate btnGodNotSelected");
+	$("#btnGodDeath").attr("class","fa fa-2x fa-low-vision btnGodNotSelected");
+	$("#btnGodAir").attr("class","fa fa-2x fa-cloud btnGodNotSelected");
+	$("#btnGodFire").attr("class","fa fa-2x fa-fire btnGodNotSelected");
+	$("#btnGodEarth").attr("class","fa fa-2x fa-tree btnGodNotSelected");
+	$("#divNewGameGodInfo").html("You've chosen: "+" "+god);
+}
+//Air
+document.getElementById("btnNewGameAir").onclick = function (){
+	$("#btnGodAir").removeClass("btnGodNotSelected");
+	$("#btnGodAir").addClass("btnGodSelected");
+	$("#btnGodAir").css("color","#b2f0ff");
+	god = "Air";
+	$("#btnGodLife, #btnGodOrder, #btnGodChaos, #btnGodWater, #btnGodDeath, #btnGodFire, #btnGodEarth").css("color","white");
+	$("#btnGodLife").attr("class","fa fa-2x fa-globe btnGodNotSelected");
+	$("#btnGodOrder").attr("class","fa fa-2x fa-balance-scale btnGodNotSelected");
+	$("#btnGodChaos").attr("class","fa fa-2x fa-certificate btnGodNotSelected");
+	$("#btnGodWater").attr("class","fa fa-2x fa-tint btnGodNotSelected");
+	$("#btnGodDeath").attr("class","fa fa-2x fa-low-vision btnGodNotSelected");
+	$("#btnGodFire").attr("class","fa fa-2x fa-fire btnGodNotSelected");
+	$("#btnGodEarth").attr("class","fa fa-2x fa-tree btnGodNotSelected");
+	$("#divNewGameGodInfo").html("You've chosen: "+" "+god);
+}
+//Fire
+document.getElementById("btnNewGameFire").onclick = function (){
+	$("#btnGodFire").removeClass("btnGodNotSelected");
+	$("#btnGodFire").addClass("btnGodSelected");
+	$("#btnGodFire").css("color","#b70909");
+	god = "Fire";
+	$("#btnGodLife, #btnGodOrder, #btnGodChaos, #btnGodWater, #btnGodAir, #btnGodDeath, #btnGodEarth").css("color","white");
+	$("#btnGodLife").attr("class","fa fa-2x fa-globe btnGodNotSelected");
+	$("#btnGodOrder").attr("class","fa fa-2x fa-balance-scale btnGodNotSelected");
+	$("#btnGodChaos").attr("class","fa fa-2x fa-certificate btnGodNotSelected");
+	$("#btnGodWater").attr("class","fa fa-2x fa-tint btnGodNotSelected");
+	$("#btnGodAir").attr("class","fa fa-2x fa-cloud btnGodNotSelected");
+	$("#btnGodDeath").attr("class","fa fa-2x fa-low-vision btnGodNotSelected");
+	$("#btnGodEarth").attr("class","fa fa-2x fa-tree btnGodNotSelected");
+	$("#divNewGameGodInfo").html("You've chosen: "+" "+god);
+}
+//Earth
+document.getElementById("btnNewGameEarth").onclick = function (){
+	$("#btnGodEarth").removeClass("btnGodNotSelected");
+	$("#btnGodEarth").addClass("btnGodSelected");
+	$("#btnGodEarth").css("color","#42703c");
+	god = "Earth";
+	$("#btnGodLife, #btnGodOrder, #btnGodChaos, #btnGodWater, #btnGodAir, #btnGodFire, #btnGodDeath").css("color","white");
+	$("#btnGodLife").attr("class","fa fa-2x fa-globe btnGodNotSelected");
+	$("#btnGodOrder").attr("class","fa fa-2x fa-balance-scale btnGodNotSelected");
+	$("#btnGodChaos").attr("class","fa fa-2x fa-certificate btnGodNotSelected");
+	$("#btnGodWater").attr("class","fa fa-2x fa-tint btnGodNotSelected");
+	$("#btnGodAir").attr("class","fa fa-2x fa-cloud btnGodNotSelected");
+	$("#btnGodFire").attr("class","fa fa-2x fa-fire btnGodNotSelected");
+	$("#btnGodDeath").attr("class","fa fa-2x fa-low-vision btnGodNotSelected");
+	$("#divNewGameGodInfo").html("You've chosen: "+" "+god);
 }
 
 //NewGame Stats
